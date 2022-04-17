787abb268d2f16a9c7a846008b37bc0ba0bfbe26
martined <edmundodeleon@gmail.com>
2022-04-17 13:25:48 -0400

    adding three todos test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,16 @@
 describe("React TodoMVC",() => {
+   beforeEach(() => {
+    cy.visit("http://localhost:8888")
+   });
+   
     it('adds a single todo', () => {
-        cy.visit("http://localhost:8888")
         cy.get(".new-todo").type("Buy milk{enter}")
         cy.get(".todo-list li").should("have.length",1)
     });
+
+    it("adds three todos",() => {
+        cy.get(".new-todo").type("Buy something{enter}")
+        cy.get(".new-todo").type("Buy things{enter}")
+        cy.get(".new-todo").type("Buy else{enter}")
+    })
 })
