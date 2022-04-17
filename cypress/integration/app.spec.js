945bc4afdb8178cf9a6d999a22fdabea37fb49c8
martined <edmundodeleon@gmail.com>
2022-04-17 13:02:57 -0400

    adding assertion

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,5 +2,6 @@ describe("React TodoMVC",() => {
     it('adds a single todo', () => {
         cy.visit("http://localhost:8888")
         cy.get(".new-todo").type("Buy milk{enter}")
+        cy.get(".todo-list li").should("have.length",1)
     });
 })
