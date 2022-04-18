800bfae0d300e7e1a0f488125a046299652c5772
Jorge Gonzalez Albisu <jorge.gonzalez.albisu@gmail.com>
2022-04-18 15:28:21 -0300

    adding a custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,11 @@
-Cypress.Commands.add("createDefaultTodos", () => {})
+Cypress.Commands.add("createDefaultTodos", () => {
+    const TODO_ITEM_ONE = "Buy Milk"
+    const TODO_ITEM_TWO = "Pay Rent"
+    const TODO_ITEM_THREE = "Pickup Dry Cleaning"
+
+    cy.get(".new-todo")
+        .type(`${TODO_ITEM_ONE}{enter}`)
+        .type(`${TODO_ITEM_TWO}{enter}`)
+        .type(`${TODO_ITEM_THREE}{enter}`)
+});
+
