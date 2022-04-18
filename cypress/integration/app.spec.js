07c59947f723fc24bd63618f66498f20c0543943
Jorge Gonzalez Albisu <jorge.gonzalez.albisu@gmail.com>
2022-04-18 14:52:57 -0300

    simple test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,7 +3,22 @@
 
 describe("React TodoMVC", () => {
 
-  it("algo", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8888");
+  })
+
+  it("adds a single todo", () => {//JORGE 
+    const inputToDoCSS = "input.new-todo";
+    const listOfToDoItemsCSS = ".todo-list > li";
+    // We need to focus on the input field.
+
+    // We need to enter in the name of our todo.
+    // We need to press the enter key to add our todo.
+    cy.get(inputToDoCSS)
+      .type("Buy Milk{enter}");
+
+
+    cy.get(listOfToDoItemsCSS).should("have.length", 1);
 
   });
 
