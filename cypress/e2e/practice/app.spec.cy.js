0380b263d0a10e41d0f03109c8455b4f605a015d
pinkmuli <98229129+pinkmuli@users.noreply.github.com>
2022-07-03 22:06:45 -0400

    Setting up Data before Each Test

--- cypress/e2e/practice/app.spec.cy.js
+++ cypress/e2e/practice/app.spec.cy.js
@@ -1,9 +1,22 @@
 describe("React TodoMVC", () => {
-  it("adds a single todo", () => {
-    cy.visit("http://localhost:8888")
-    cy.get(".new-todo").type("Study Github {enter}")
-    cy.get(".new-todo").type("feed the fur children {enter}")
+  const TODO_ITEM_ONE = "Fold the laundry"
+  const TODO_ITEM_TWO = "Study Github"
+  const TODO_ITEM_THREE = "feed the fur children"
 
-    cy.get(".todo-list li").should("have.length", 2)
+  beforeEach(() => {
+    cy.visit("http://localhost:8888")
+  })
+  it("adds a single todo", () => {
+    cy.get(".new-todo").type(`${TODO_ITEM_ONE}{enter}`)
+    cy.get(".todo-list li").should("have.length", 1)
+  })
+
+  it("adds three todos", () => {
+    cy.get(".new-todo").type(`${TODO_ITEM_ONE}{enter}`)
+    cy.get(".new-todo").type(`${TODO_ITEM_TWO}{enter}`)
+    cy.get(".new-todo").type(`${TODO_ITEM_THREE}{enter}`)
+
+    cy.get(".todo-list li").should("have.length", 3)
+    cy.get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
   })
 })
